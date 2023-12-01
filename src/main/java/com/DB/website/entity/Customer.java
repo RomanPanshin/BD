bf58461ec2092a19e20c777f02ccf45d5d4c6673
Roman Panshin <roma.super@icloud.com>
2023-12-01 11:40:24 +0300

    Inital commit

---
+++ src/main/java/com/DB/website/entity/Customer.java
@@ -0,0 +1,75 @@
+package com.DB.website.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer customer_id;
+
+    @Column(name = "branch_id")
+    private Integer branch_id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+
+    @Column(name = "profession")
+    private String profession;
+
+    @Column(name = "sex", nullable = false)
+    private String sex;
+
+    public Integer getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(Integer customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public Integer getBranch_id() {
+        return branch_id;
+    }
+
+    public void setBranch_id(Integer branch_id) {
+        this.branch_id = branch_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
